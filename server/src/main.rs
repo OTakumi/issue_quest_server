d1d91b8e3ff0bfc518456bb6d54cc2e4cb9bf57f
OTakumi <takumi.o.0410@gmail.com>
2025-01-03 22:17:01 +0900

    feat: add rocket

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::launch;
+use rocket::routes;
+use rocket::*;
+
+mod domain;
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
